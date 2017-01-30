43cc23d8631fe779ed220d34adcf662cf7eb1df0
Ronnel Bautista <ronnel.j.bautista@gmail.com>
2017-01-30 13:13:51 -0800

    Time assignment

---
+++ time.js
@@ -0,0 +1,21 @@
+//Create a program that will tell you the time.
+//It should log "It's almost 9 in the morning"
+function time(){
+var hour = 8;
+var minute = 50;
+var period = "am";
+
+console.log("It's almost 9 in the morning")
+}
+time()
+
+//it should log "It's just after 7 in the evening"
+function time1(){
+
+var hour = 7;
+var minute = 15;
+var period = "pm";
+
+console.log("It's just after 7 in the evening")
+}
+time1()
